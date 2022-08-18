1c86ac68ada78f1ab1cfe12955165f719f3924ce
lkasdj02 <leonardo.basso02@gmail.com>
2022-08-18 10:43:48 +0200

    aggiunta la funzione di ricerca di un nodo all'interno dell'albero

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct btree {
   int valore;
@@ -11,8 +12,13 @@ typedef struct btree {
 void init(BTree **ptrPtr); // funzione che sfrutta l'allocazione dinamica della memoria.
 int isemtpy(BTree *ptr);
 int search_rec(BTree *ptr, int val);
-int print_rec(BTree *ptr, char *opt);
+void print_rec(BTree *ptr, char *opt);
 void ordinsert_rec(BTree **ptrPtr, int val);
+
+// opzioni globali.
+char simmetrica[20]  = "simmetrica"; 
+char pre_ordine[20]  = "pre_ordine"; // anche definita come anticipata.
+char post_ordine[20] = "post_ordine"; // anche definita come posticipata.
 
 int main() {
 
@@ -20,8 +26,19 @@ int main() {
   init(&albero);
   (isemtpy(albero)) ? printf("albero vuoto\n") : printf("albero NON vuoto\n");
 
-  ordinsert_rec(&albero, 3);
+  ordinsert_rec(&albero, 2);
   (isemtpy(albero)) ? printf("albero vuoto\n") : printf("albero NON vuoto\n");
+
+  // inserimento di un paio di nodi.
+  ordinsert_rec(&albero, 1);
+  ordinsert_rec(&albero, 6);
+  ordinsert_rec(&albero, 0);
+  
+  print_rec(albero, post_ordine);
+  printf("\n");
+
+  int target = 7;
+  printf("\nL'albero contiene %d? %d\n", target, search_rec(albero, target));
 }
 
 void init(BTree **ptrPtr) {
@@ -30,6 +47,21 @@ void init(BTree **ptrPtr) {
 
 int isemtpy(BTree *ptr) {
   return (ptr == NULL);
+}
+
+int search_rec(BTree *ptr, int val) {
+  if (isemtpy(ptr)) { // caso base: sotto albero vuoto = radice NULLA.
+      return -1;
+  } else {
+    if (ptr->valore == val)
+      return 1;
+    else {     
+      if (ptr->valore < val)
+        return search_rec(ptr->rightPtr, val);
+      if (ptr->valore > val)
+        return search_rec(ptr->leftPtr, val);
+    }
+  } 
 }
 
 void ordinsert_rec(BTree **ptrPtr, int val) {
@@ -48,3 +80,19 @@ void ordinsert_rec(BTree **ptrPtr, int val) {
   }
 }
 
+void print_rec(BTree *ptr, char *opt) {
+  if (ptr != NULL) {
+    if (strcmp("pre_ordine", opt) == 0)
+      printf("%d ", ptr->valore);
+
+    print_rec(ptr->leftPtr, opt);
+
+    if (strcmp("simmetrica", opt) == 0)
+      printf("%d ", ptr->valore);
+
+    print_rec(ptr->rightPtr, opt);
+
+    if (strcmp("post_ordine", opt) == 0)
+      printf("%d ", ptr->valore);
+  }
+}
